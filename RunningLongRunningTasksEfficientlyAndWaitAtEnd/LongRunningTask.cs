bf5198219dc563dfcfe75c39f8727fad00ea884b
Raja Brij Bhushan <rajabb@gmail.com>
2022-08-21 13:51:22 +0530

    Add files via upload

---
+++ RunningLongRunningTasksEfficientlyAndWaitAtEnd/LongRunningTask.cs
@@ -0,0 +1,12 @@
+﻿namespace RunningLongRunningTasksEfficientlyAndWaitAtEnd;
+
+public class LongRunningTask
+{
+    public static async Task RunAsync(string input)
+    {
+        Random rnd = new Random();
+        await Task.Delay(rnd.Next(1000,10000));
+        Console.WriteLine($"Task {input} Done.");
+    }
+}
+
---
+++ RunningLongRunningTasksEfficientlyAndWaitAtEnd/Program.cs
@@ -0,0 +1,15 @@
+﻿using RunningLongRunningTasksEfficientlyAndWaitAtEnd;
+
+ Console.WriteLine("Running Task");
+
+ List<Task> tasks = new List<Task>();
+
+ for (int i = 0; i < 100; i++)
+ {
+      tasks.Add(LongRunningTask.RunAsync(i.ToString()));
+ }
+
+ await Task.WhenAll(tasks.ToArray());
+
+ Console.WriteLine("Approach 1 Done");
+
